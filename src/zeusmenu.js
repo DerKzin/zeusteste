f6213e8cad05a95a20553b5664146d8b5751d5d3
testezeus <thiagoflix77@gmail.com>
2021-01-27 12:20:50 -0400

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Zeus:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *ZEUS*
+  DUVIDAS? 👇
+  WA.me/556993899391
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
